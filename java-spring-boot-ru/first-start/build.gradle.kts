fa1641fc08ba758947b9799c78ca999a70413ad3
@hexlet/cli <support@hexlet.io>
2025-05-05 17:32:40 +0300

    submit java-spring-boot-ru/first-start

--- java-spring-boot-ru/first-start/build.gradle.kts
+++ java-spring-boot-ru/first-start/build.gradle.kts
@@ -3,7 +3,9 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     // BEGIN
-    
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.3"
     // END
     application
     id("com.github.ben-manes.versions") version "0.48.0"
