37284799d271d9e5124b031c648fb04f85dd81c8
@hexlet/cli <support@hexlet.io>
2025-05-09 13:51:57 +0300

    submit java-spring-boot-ru/http-crud

---
+++ java-spring-boot-ru/http-crud/src/main/java/exercise/Application.java
@@ -0,0 +1,68 @@
+package exercise;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // BEGIN
+    @GetMapping("/pages") // Список страниц
+    public List<Post> index(@RequestParam(defaultValue = "10") Integer limit) {
+        return posts.stream().limit(limit).toList();
+    }
+
+    @PostMapping("/pages") // Создание страницы
+    public Post create(@RequestBody Post page) {
+        posts.add(page);
+        return page;
+    }
+
+    @GetMapping("/pages/{id}") // Вывод страницы
+    public Optional<Post> show(@PathVariable String id) {
+        var page = posts.stream()
+                        .filter(p -> p.getSlug().equals(id))
+                        .findFirst();
+        return page;
+    }
+
+    @PutMapping("/pages/{id}") // Обновление страницы
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var maybePage = posts.stream()
+                             .filter(p -> p.getSlug().equals(id))
+                             .findFirst();
+        if (maybePage.isPresent()) {
+            var page = maybePage.get();
+            page.setSlug(data.getSlug());
+            page.setName(data.getName());
+            page.setBody(data.getBody());
+        }
+        return data;
+    }
+
+    @DeleteMapping("/pages/{id}") // Удаление страницы
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getSlug().equals(id));
+    }
+    // END
+}
