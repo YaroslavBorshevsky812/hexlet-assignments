2193303b47440cd5b5519b7f458447b1c58be236
@hexlet/cli <support@hexlet.io>
2025-05-21 21:42:06 +0300

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/src/main/java/exercise/controller/UserController.java
+++ java-spring-boot-ru/authentication/src/main/java/exercise/controller/UserController.java
@@ -24,35 +24,34 @@ import exercise.exception.ResourceNotFoundException;
 public class UserController {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
     @Autowired
     private UserMapper userMapper;
 
-    @GetMapping("/users")
-    ResponseEntity<List<UserDTO>> index() {
-        var users = repository.findAll();
-        var result = users.stream()
-                          .map(userMapper::map)
-                          .toList();
-        return ResponseEntity.ok()
-                             .header("X-Total-Count", String.valueOf(users.size()))
-                             .body(result);
+    @GetMapping(path = "")
+    public List<UserDTO> index() {
+        var users = userRepository.findAll();
+        return users.stream()
+                    .map(p -> userMapper.map(p))
+                    .toList();
     }
 
-    @PostMapping("/users")
+    @GetMapping(path = "/{id}")
+    public UserDTO show(@PathVariable long id) {
+
+        var user =  userRepository.findById(id)
+                                  .orElseThrow(() -> new ResourceNotFoundException("User with id " + id + " not found"));
+        var userDto = userMapper.map(user);
+        return userDto;
+    }
+
+    @PostMapping(path = "")
     @ResponseStatus(HttpStatus.CREATED)
-    UserDTO create(@Valid @RequestBody UserCreateDTO userData) {
+    public UserDTO create(@Valid @RequestBody UserCreateDTO userData) {
         var user = userMapper.map(userData);
-        repository.save(user);
-        return userMapper.map(user);
-    }
-
-    @GetMapping("/users/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    UserDTO show(@PathVariable Long id) {
-        var user = repository.findById(id)
-                             .orElseThrow(() -> new ResourceNotFoundException("Not Found"));
-        return userMapper.map(user);
+        userRepository.save(user);
+        var userDto = userMapper.map(user);
+        return userDto;
     }
 }
