13fd74a39f615f94c4a48530a39affc0e84fe978
@hexlet/cli <support@hexlet.io>
2025-05-21 15:43:42 +0300

    submit java-spring-boot-ru/crud

---
+++ java-spring-boot-ru/crud/src/main/java/exercise/mapper/ReferenceMapper.java
@@ -0,0 +1,25 @@
+package exercise.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.TargetType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import exercise.model.BaseEntity;
+import jakarta.persistence.EntityManager;
+
+// BEGIN
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public class ReferenceMapper {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
+        if (id == null) {
+            return null;
+        }
+        return entityManager.find(entityClass, id);
+    }
+}
+// END
