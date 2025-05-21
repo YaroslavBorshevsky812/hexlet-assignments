5c35e0a9c5f223baaf8931bd6bba2888ced63a03
@hexlet/cli <support@hexlet.io>
2025-05-21 20:50:33 +0300

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package exercise.service;
 
-import exercise.model.User;
 import exercise.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,49 +9,5 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 // BEGIN
-@Service
-public class CustomUserDetailsService implements UserDetailsManager {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
-                             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    }
-
-    @Override
-    public void createUser(UserDetails user) {
-        User newUser = (User) user;
-        newUser.setPasswordDigest(passwordEncoder.encode(newUser.getPassword()));
-        userRepository.save(newUser);
-    }
-
-    @Override
-    public void updateUser(UserDetails user) {
-        User updatedUser = (User) user;
-        userRepository.save(updatedUser);
-    }
-
-    @Override
-    public void deleteUser(String email) {
-        User user = userRepository.findByEmail(email)
-                                  .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        userRepository.delete(user);
-    }
-
-    @Override
-    public void changePassword(String oldPassword, String newPassword) {
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    @Override
-    public boolean userExists(String email) {
-        return userRepository.findByEmail(email).isPresent();
-    }
-}
 // END
