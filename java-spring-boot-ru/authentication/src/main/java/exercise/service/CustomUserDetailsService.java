64030791046a7045f75c4a43f748cab2b00c960e
@hexlet/cli <support@hexlet.io>
2025-05-21 20:59:23 +0300

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
+++ java-spring-boot-ru/authentication/src/main/java/exercise/service/CustomUserDetailsService.java
@@ -9,5 +9,47 @@ import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 // BEGIN
+@Service
+public class CustomUserDetailsService implements UserDetailsManager {
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+        User newUser = (User) user;
+        newUser.setPasswordDigest(passwordEncoder.encode(newUser.getPassword()));
+        userRepository.save(newUser);
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        User existingUser = (User) user;
+        userRepository.save(existingUser);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        User user = (User) loadUserByUsername(username);
+        userRepository.delete(user);
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        // Implementation not needed for this exercise
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return userRepository.findByEmail(username).isPresent();
+    }
+}
 // END
